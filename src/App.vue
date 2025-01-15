6e0c58f1368813a77aa8252c1d62bc35ddbb69b7
Pascal Luxain <pascal.luxain@gmail.com>
2025-01-15 17:33:05 +0100

    feat(content): add simple home to check TailwindCSS is correctly integrated
    Ref: #1

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,7 @@
-<script setup lang="ts">
-import HelloWorld from "./components/HelloWorld.vue";
-</script>
-
 <template>
-  <div>
-    <a href="https://vite.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
-  </div>
-  <HelloWorld msg="Vite + Vue" />
+  <section class="m-auto flex h-screen w-1/2 items-center justify-center">
+    <h1 class="text-center text-4xl font-bold">
+      <acronym title="Check-Lists are not Todo-Lists">cnantl</acronym>
+    </h1>
+  </section>
 </template>
-
-<style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
-</style>
