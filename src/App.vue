ecc2d6bff91d25345074c12e8660f785d0cc10aa
Pascal Luxain <pascal.luxain@gmail.com>
2025-01-20 16:19:54 +0100

    fix(content): fix typo in title
    Ref: #11

--- src/App.vue
+++ src/App.vue
@@ -4,7 +4,7 @@ import { CheckList } from "@features/checklist";
 <template>
   <main class="m-auto flex h-screen w-1/2 flex-col">
     <h1 class="mb-4 text-center text-4xl font-bold">
-      <abbr title="Check-Lists are not Todo-Lists">cnantl</abbr>
+      <abbr title="Check-Lists are not Todo-Lists">clantl</abbr>
     </h1>
     <CheckList />
   </main>
