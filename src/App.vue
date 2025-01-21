76a0af4ca1f034713f6e2aaabedcc8c51329bf15
Pascal Luxain <pascal.luxain@gmail.com>
2025-01-21 18:00:31 +0100

    feat(design): make check-list full width
    Ref: #11

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,7 @@
 import { CheckList } from "@features/checklist";
 </script>
 <template>
-  <main class="m-auto flex h-screen w-1/2 flex-col">
+  <main class="flex h-screen w-screen flex-col p-4">
     <h1 class="mb-4 text-center text-4xl font-bold">
       <abbr title="Check-Lists are not Todo-Lists">clantl</abbr>
     </h1>
