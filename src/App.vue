97882bc017501f1da8d7a96a1306ac6aa45aba71
Pascal Luxain <pascal.luxain@gmail.com>
2025-01-22 07:58:09 +0100

    feat(content): remove title to save space
    Ref: #11

--- src/App.vue
+++ src/App.vue
@@ -2,10 +2,7 @@
 import { CheckList } from "@features/checklist";
 </script>
 <template>
-  <main class="flex h-screen w-screen flex-col p-4">
-    <h1 class="mb-4 text-center text-4xl font-bold">
-      <abbr title="Check-Lists are not Todo-Lists">clantl</abbr>
-    </h1>
+  <main class="flex w-full flex-col p-4">
     <CheckList />
   </main>
 </template>
