f33d28ac9c35cda8adf79f5213aa567f626fa406
Pascal Luxain <pascal.luxain@gmail.com>
2025-01-30 09:09:49 +0100

    feat(content): add a Home view and its route
    Ref: #17

---
+++ src/features/home/HomeView.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+import * as t from "@locales/messages";
+</script>
+<template>
+  <section class="mx-4 mt-8 md:mx-36">
+    <h1 class="text-center text-4xl">
+      <abbr :title="t.clantl_moto()">clantl</abbr>
+    </h1>
+    <div class="mt-8 md:text-lg">
+      <RouterLink :to="{ name: 'checklist' }" class="underline">{{
+        t.checklist_link()
+      }}</RouterLink>
+    </div>
+  </section>
+</template>
--- src/routing/routes.ts
+++ src/routing/routes.ts
@@ -1,8 +1,10 @@
 // Try to use [dynamic imports](https://router.vuejs.org/guide/advanced/lazy-loading.html#Lazy-Loading-Routes) as much as possible
+const HomeView = () => import("@features/home");
 const CheckList = () => import("@features/checklist");
 
 const routes = [
-  { path: "/", name: "checklist", component: CheckList },
+  { path: "/", name: "home", component: HomeView },
+  { path: "/checklist", name: "checklist", component: CheckList },
 ] as const;
 
 export { routes };
