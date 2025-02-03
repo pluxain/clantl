5f756a96977decb923add7e3f0760c569d756685
Pascal Luxain <pascal.luxain@gmail.com>
2025-02-03 08:53:01 +0100

    feat(content): display locales on home page more clearly
    Ref: #17

--- src/features/home/HomeView.vue
+++ src/features/home/HomeView.vue
@@ -1,20 +1,28 @@
 <script setup lang="ts">
 import * as t from "@locales/messages";
 import { availableLanguageTags } from "@locales/runtime";
+import { ClantlLocale } from "@components";
 </script>
 <template>
   <section class="mx-4 mt-8 md:mx-36">
     <h1 class="text-center text-4xl">
       <abbr :title="t.clantl_moto()">clantl</abbr>
     </h1>
-    <div class="mt-8 flex gap-2 md:text-lg">
-      <RouterLink
-        v-for="languageTag in availableLanguageTags"
-        :key="languageTag"
-        :to="{ name: 'checklist', params: { locale: languageTag } }"
-        class="underline"
-        >{{ t.checklist_link(undefined, { languageTag }) }}</RouterLink
-      >
+    <div class="mt-8 flex flex-col gap-2 md:text-lg">
+      <div v-for="locale in availableLanguageTags" :key="locale">
+        <h2 class="my-4 text-3xl font-bold">
+          <ClantlLocale :locale="locale" />
+        </h2>
+        <div>
+          <RouterLink
+            :to="{ name: 'checklist', params: { locale } }"
+            class="underline"
+            >{{
+              t.checklist_link(undefined, { languageTag: locale })
+            }}</RouterLink
+          >
+        </div>
+      </div>
     </div>
   </section>
 </template>
