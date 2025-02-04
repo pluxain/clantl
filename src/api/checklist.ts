f24993aab6ec6e517b2d62c10a7234a644debbb5
Pascal Luxain <pascal.luxain@gmail.com>
2025-02-04 15:13:16 +0100

    feat(content): fetch checklist from JSON file
    Ref: #19

--- src/api/checklist.ts
+++ src/api/checklist.ts
@@ -1,47 +1,13 @@
 import type { Checklist, Realm, Step } from "@types";
 
 export async function getChecklist(
-  _realm: Realm,
-  _step: Step,
+  realm: Realm,
+  step: Step,
 ): Promise<Checklist> {
   // Simulate delay
   await new Promise((resolve) => setTimeout(resolve, 1000));
-
-  return {
-    items: [
-      {
-        label: "matériel d'intubation",
-        keyword: "vérifié",
-        verified: false,
-      },
-      { label: "oxygène", keyword: "disponible", verified: false },
-      {
-        label: "circuit / Ballon de réanimation",
-        keyword: "testé",
-        verified: false,
-      },
-      { label: "valve d'échappement", keyword: "Ouverte", verified: false },
-      {
-        label: "ballonnet sonde endotrachéale",
-        keyword: "étanche",
-        verified: false,
-      },
-      { label: "cathéter", keyword: "fonctionnel", verified: false },
-      {
-        label: "procédure de réanimation",
-        keyword: "prête",
-        verified: false,
-      },
-      {
-        label: "responsable de la surveillance",
-        keyword: "nommé",
-        verified: false,
-      },
-    ],
-    locale: "fr",
-    nextStep: "induction",
-    realm: "anesthesie",
-    resetCount: 0,
-    step: "avant-induction",
-  };
+  // Note: for now we do not consider locale as a level of depth.
+  // Actually, the realm and step should be localized.
+  const res = await fetch(`/data/${realm}/${step}/checklist.json`);
+  return res.json();
 }
