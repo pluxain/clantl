802703c16d23a0cef22c8cb67903056dcf66b6b9
Pascal Luxain <pascal.luxain@gmail.com>
2025-02-10 19:53:35 +0100

    feat(design): add visual indicator on KillerItem when verified
    Ref: #24

--- src/features/flow/CheckList.vue
+++ src/features/flow/CheckList.vue
@@ -90,13 +90,16 @@ function listReset() {
           @click="check(index)"
         >
           <dd
-            class="label grid grid-cols-[auto_1fr] gap-4 p-4 font-bold uppercase"
+            class="label grid grid-cols-[40px_auto_1fr] gap-4 p-4 font-bold uppercase"
             :class="[
               item.verified
                 ? 'border-success text-success border-b'
                 : 'border-secondary border-b text-black',
             ]"
           >
+            <div class="flex items-center">
+              <span v-if="item.verified">{{ "\u2713" }}</span>
+            </div>
             <div class="flex items-center">{{ item.label }}</div>
             <div class="overflow-hidden">
               {{ "&nbsp;\u2218&nbsp;".repeat(100) }}
