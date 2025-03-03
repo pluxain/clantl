fe50a48cf9702d0538b0cd38d382c16090a21386
Pascal Luxain <pascal.luxain@gmail.com>
2025-03-03 09:13:54 +0100

    feat(tech): check TS on staged file
    Ref: #44

--- lint-staged.config.mjs
+++ lint-staged.config.mjs
@@ -1,3 +1,7 @@
 export default {
-  "*.{vue,md,mjs,ts,tsx,html,json}": ["npx @biomejs/biome check --write"],
+  "*.{vue,ts,tsx}": [
+    () => "npx vue-tsc -p tsconfig.app.json --noEmit --pretty",
+    "npx @biomejs/biome check --write",
+  ],
+  "*.{md,mjs,html,json}": ["npx @biomejs/biome check --write"],
 };
