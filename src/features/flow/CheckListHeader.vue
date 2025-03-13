07c170948ffb83026540683de8c2330164e14716
Pascal Luxain <pascal.luxain@gmail.com>
2025-03-13 22:12:56 +0100

    ref(content): embed actions in CheckListHeader
    - remove CheckListActionBar Component
    
    Ref: #55

--- src/features/flow/CheckListHeader.vue
+++ src/features/flow/CheckListHeader.vue
@@ -1,4 +1,7 @@
 <script setup lang="ts">
+import { RouterLink } from "vue-router";
+import { ClantlButton } from "#components";
+import * as t from "#locales/messages";
 import type { Realm, Step } from "#types";
 
 type Props = {
@@ -8,17 +11,38 @@ type Props = {
 };
 
 defineProps<Props>();
+
+const emit = defineEmits<(e: "reset") => void>();
 </script>
 
 <template>
-  <div class="header bg-warning p-4 text-white uppercase">
-    <h2 class="flex items-center justify-center gap-4 text-3xl">
-      <span class="realm">{{ realm }}</span>
-      <span class="step">{{ step }}</span>
-    </h2>
-    <h4 class="mt-4 text-center text-4xl font-bold">
-      <span class="name">{{ name }}</span>
-    </h4>
+  <div class="header grid grid-cols-8 bg-warning p-4 text-white uppercase">
+    <div class="flex items-center justify-start font-bold text-4xl">
+      <RouterLink :to="{ name: 'home' }" :title="t.go_back_home()">
+        {{ "\u27ea" }}
+      </RouterLink>
+    </div>
+
+    <div class="col-span-6">
+      <h2 class="flex items-center justify-center gap-4 text-3xl">
+        <span class="realm">{{ realm }}</span>
+        <span class="step">{{ step }}</span>
+      </h2>
+      <h4 class="mt-4 text-center text-4xl font-bold">
+        <span class="name">{{ name }}</span>
+      </h4>
+    </div>
+
+    <div class="flex items-center justify-end font-bold text-4xl">
+      <ClantlButton
+        severity="primary"
+        :hint="t.btn_reset_checklist_hint()"
+        type="button"
+        @click="emit('reset')"
+      >
+        {{ "\u21BB" }}
+      </ClantlButton>
+    </div>
   </div>
 </template>
 
--- src/features/flow/index.ts
+++ src/features/flow/index.ts
@@ -1,15 +1,8 @@
 import CheckList from "./CheckList.vue";
-import CheckListActionBar from "./CheckListActionBar.vue";
 import CheckListCompleted from "./CheckListCompleted.vue";
 import CheckListHeader from "./CheckListHeader.vue";
 import CheckListKillerItem from "./CheckListKillerItem.vue";
 import FeatureView from "./FeatureView.vue";
 
-export {
-  CheckList,
-  CheckListActionBar,
-  CheckListCompleted,
-  CheckListHeader,
-  CheckListKillerItem,
-};
+export { CheckList, CheckListCompleted, CheckListHeader, CheckListKillerItem };
 export default FeatureView;
